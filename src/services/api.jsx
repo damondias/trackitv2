9888cdbe607ff8870e91694917a82a434a931af7
Damon <damondias@gmail.com>
2023-06-05 12:37:09 -0400

    fix: Base URL correct

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/';
+const BASE_URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit';
 
 
 function createConfig(token) {
